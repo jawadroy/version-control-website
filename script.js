54a040c206f3f0952c671ecf0b3e8a4216eca1f3
jawadroy <mj8117542@gmail.com>
2024-09-05 18:25:11 -0700

    creation attractive navbar

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Simulate a delay for demonstration purposes
+    setTimeout(function() {
+        document.body.classList.add('loaded');
+    }, 6000); // Change the duration to suit your needs
+});
